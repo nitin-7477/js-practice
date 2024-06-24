76f5d52894e2df4853be8a2ee10880a507b5704e
nitin-7477 <gautam.nitinji@gmail.com>
2024-06-25 00:01:55 +0530

    swap variable without using third variable

--- que4.js
+++ que4.js
@@ -1,3 +1,5 @@
+// swap two variable with temperary variable
+
 function call(a, b) {
   // take a temporary variable to store the value of a so that we can use it later 
   //now store the value of a in temporary value and get the value of b in a 
@@ -9,3 +11,14 @@ function call(a, b) {
 
 }
 call(9, 76)
+
+// swap without using any third variable
+
+function swapVariable(a,b) {
+  a= a+b
+  b=a-b
+  a=a-b
+  console.log(a,b)
+  
+}
+swapVariable(28,92)
