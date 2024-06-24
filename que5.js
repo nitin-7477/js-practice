5972a64668a90cf97b65f16510011d05c0751bfa
nitin-7477 <gautam.nitinji@gmail.com>
2024-06-25 00:08:48 +0530

    sum of natural number with loop

---
+++ que5.js
@@ -0,0 +1,11 @@
+//program to find the sum of natural numbers
+function sumOfNatural(n) {
+  let sum = 0;
+  for(let i=1;i<=n;i++)
+    {
+      sum+=i;
+    }
+    console.log(sum)
+  
+}
+sumOfNatural(4)
