140e1e5986a7bb7fa94477abfd341619ac7449be
nitin-7477 <gautam.nitinji@gmail.com>
2024-06-24 23:40:47 +0530

    even/odd by module operator

---
+++ que2.js
@@ -0,0 +1,14 @@
+//javascript progarm to check whehter a number is odd or even
+
+function  call(num) {
+if(num%2==0)
+  {
+    return 'even'
+  }
+  else{
+    return 'odd'
+  }
+  
+}
+console.log(call(9))
+console.log(call(8))
